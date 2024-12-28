222f5e51d5b8b23fa5e6a9a0f755246c21c95d03
trflorian <florian.trautweiler@hispeed.ch>
2024-12-28 14:51:43 +0100

    add image processing function

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from pathlib import Path
 from tqdm import tqdm
 
+import cv2
+
+def process_image(input_path: Path, output_path: Path):
+    img = cv2.imread(str(input_path))
+
+    # Crop left/right black border
+    border_width = 270
+    img = img[:, border_width:-border_width, :]
+
+    # Resize to fixed size
+    target_size = 224
+    img = cv2.resize(img, (target_size, target_size))
+
+    # Save processed image
+    cv2.imwrite(str(output_path), img)
+
 # INPUT
 img_paths = Path("images").glob("*.png")
 
@@ -9,4 +25,7 @@ output_path = Path("output")
 output_path.mkdir(exist_ok=True, parents=True)
 
 for img_path in tqdm(sorted(img_paths)):
-    pass
+    process_image(
+        input_path=img_path,
+        output_path=output_path / img_path.name
+    )
